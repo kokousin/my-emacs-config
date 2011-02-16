706bf34d3913ddb23574afc1ac6829f14f07214e
guoxx <gxiaoc@gmail.com>
2011-02-16 14:36:51 +0800

    backup .emacs

--- config/emacs.init/shell.el
+++ config/emacs.init/shell.el
@@ -4,7 +4,7 @@
 (global-set-key "\C-x," 'multi-term-dedicated-open)
 
 (custom-set-variables
-  '(term-default-bg-color "#dddddd")        ;; background color (black)
-  '(term-default-fg-color "#000000"))       ;; foreground color (yellow)
+  '(term-default-bg-color "#000000")        ;; background color (black)
+  '(term-default-fg-color "#0000dd"))       ;; foreground color (blue)
 
 (setq multi-term-dedicated-select-after-open-p t)
