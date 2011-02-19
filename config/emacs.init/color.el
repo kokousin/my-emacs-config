b1f74a9ace08a3cf33880da4914ed752a592ffcf
guoxx <gxiaoc@gmail.com>
2011-02-19 15:57:12 +0800

    change color theme

--- config/emacs.init/color.el
+++ config/emacs.init/color.el
@@ -1,6 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/el-get/color-theme/")
 (require 'color-theme)
 (color-theme-initialize)
-(load-file "~/.emacs.d/el-get/color-theme-twilight/color-theme-twilight.el")
+;;(load-file "~/.emacs.d/el-get/color-theme-twilight/color-theme-twilight.el")
 
-(color-theme-twilight)
+;;(color-theme-twilight)
+(color-theme-charcoal-blac)
